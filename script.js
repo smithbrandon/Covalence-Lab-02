9ba5961c11212fa8559ef7a8a6a30e8848a234f8
smithbrandon <smith.brandon.e.82@gmail.com>
2017-06-01 09:11:34 -0400

    refactored to reduce code

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 var friends = ['Bill','Bob','Jim','Jimmy','Charles'];
 for (var i = 0; i< friends.length; i++){
     console.log("\n" + friends[i] + ": ");
-    console.log("");
-    for (var j = 99; j > 0; j--){
-        var song = "";
-        if(j === 1){
-            song+= " " + String(j) + " line of code in the file, "+ String(j) + " line of code;  "+ friends[i]+  " strikes one out, clears it all out, no more lines of code in the file.";            
-        }else{
-            song+= " " + String(j) + " lines of code in the file "+String(j)+" lines of code;  " + friends[i] + " strikes one out, clears it all out," + String(j-1) + " more lines of code in the file.";
-        }
-        console.log(song);
+    for (var j = 99; j > 1; j--){
+        console.log(" " + String(j) + " lines of code in the file "+String(j)+" lines of code;  " + friends[i] + " strikes one out, clears it all out," + String(j-1) + " more lines of code in the file.");
     }
+   console.log(" 1 line of code in the file, 1 line of code;  "+ friends[i]+  " strikes one out, clears it all out, no more lines of code in the file.");            
 }
