2a8efd6747a4a6b4e3ecd4978e91b00a20e9ecce
smithbrandon <smith.brandon.e.82@gmail.com>
2017-06-01 09:41:48 -0400

    fixed issue with grammer for 1 line(s) of code

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var friends = ['Bill','Bob','Jim','Jimmy','Charles'];
 for (var i = 0; i< friends.length; i++){
     console.log("\n" + friends[i].toUpperCase() + ": ");
-    for (var j = 99; j > 1; j--){
-        console.log(" " + String(j) + " lines of code in the file "+String(j)+" lines of code;  " + friends[i] + " strikes one out, clears it all out," + String(j-1) + " more lines of code in the file.");
+    for (var j = 99; j > 0; j--){
+        if(j>2){
+            console.log(" " + j + " lines of code in the file "+j+" lines of code, " + friends[i] + " strikes one out, clears it all out," + (j-1) + " more lines of code in the file.");
+        }else if(j === 2){
+            console.log(" " + j + " lines of code in the file " + j + " lines of code, " + friends[i] + " strikes one out, clears it all out," + (j-1) + " more line of code in the file.");
+        }else{
+           console.log(" " + j +  " line of code in the file, " + j + " line of code,  "+ friends[i]+  " strikes one out, clears it all out, no more lines of code in the file.");           
+        }
     }
-   console.log(" 1 line of code in the file, 1 line of code;  "+ friends[i]+  " strikes one out, clears it all out, no more lines of code in the file.");            
 }
